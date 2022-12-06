6f308032d9cf12fb65d1b70bae2005d313382f15
thuhuyen <nguyenthuhuyen@gmail.com>
2022-12-06 23:16:26 +0700

    update contact page

---
+++ TLS.Web/Controllers/ContactController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        [Route("/contact")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
