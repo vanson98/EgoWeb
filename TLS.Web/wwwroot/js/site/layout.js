f5ceabec7189e50dcc35edf4881544f54531cce7
thuhuyen <nguyenthuhuyen@gmail.com>
2022-12-06 21:59:15 +0700

    update ui

---
+++ TLS.Web/wwwroot/js/site/layout.js
@@ -0,0 +1,11 @@
+﻿const toggleBtn = document.querySelector(".sidebar-toggle");
+const closebtn = document.querySelector(".close-btn");
+const sidebar = document.querySelector(".sidebar");
+
+toggleBtn.addEventListener("click", function () {
+    sidebar.classList.toggle("show-sidebar");
+});
+
+closebtn.addEventListener("click", function () {
+    sidebar.classList.remove("show-sidebar");
+});
