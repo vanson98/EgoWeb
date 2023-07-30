a2eab1a1e9d44b98be5b9d6e5a00996bbe4e8f9f
vanson <vansonnguyen159@outlook.com>
2023-07-30 15:01:07 +0700

    update

---
+++ TLS.Web/Controllers/SolutionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class SolutionController : Controller
+    {
+
+        [Route("/solution")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
