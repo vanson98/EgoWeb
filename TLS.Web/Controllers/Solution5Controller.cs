082b23f6c76f4831688b231d49d29868b1027980
thuhuyen <nguyenthuhuyen@gmail.com>
2023-08-19 16:39:41 +0700

    update solution 5

---
+++ TLS.Web/Controllers/Solution5Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class Solution5Controller : Controller
+    {
+
+        [Route("/solution-5")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
