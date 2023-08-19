3f0e62928d393ad2d1cd7beead7b8b62652ded73
thuhuyen <nguyenthuhuyen@gmail.com>
2023-08-19 17:46:45 +0700

    update solution page 2

---
+++ TLS.Web/Controllers/Solution3Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class Solution3Controller : Controller
+    {
+
+        [Route("/solution-3")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
