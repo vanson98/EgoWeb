bad9fd19ff7c58f6792296c586ca73591c1c9e08
thuhuyen <nguyenthuhuyen@gmail.com>
2023-08-20 18:05:17 +0700

    update solution 2

---
+++ TLS.Web/Controllers/Solution2Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class Solution2Controller : Controller
+    {
+
+        [Route("/solution-2")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
