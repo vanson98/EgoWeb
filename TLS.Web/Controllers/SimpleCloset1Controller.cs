4b1879a41179a1f8646f8335f9a7cd6b4768aa4d
thuhuyen <nguyenthuhuyen@gmail.com>
2023-09-09 17:23:21 +0700

    update closet page

---
+++ TLS.Web/Controllers/SimpleCloset1Controller.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class SimpleCloset1Controller : Controller
+    {
+        [Route("/closet-1")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TLS.Web/Controllers/SimpleCloset1Controller2.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class SimpleCloset2Controller : Controller
+    {
+        [Route("/closet-2")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
