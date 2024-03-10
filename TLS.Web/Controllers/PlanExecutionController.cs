8764bd057a34b5450c46e3af0b851ca4c8b53c67
vanson <vansonnguyen159@outlook.com>
2024-03-10 09:09:09 +0700

    Add layout web 2

---
+++ TLS.Web/Controllers/PlanExecutionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TLS.Web.Controllers
+{
+    public class PlanExecutionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
